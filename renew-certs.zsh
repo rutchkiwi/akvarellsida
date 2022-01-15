935ce3982992d41dcdfce6f9b64463342b63fbc3
Viktor <viktor@medino.com>
2022-01-15 21:48:54 +0000

    add helenas website nginx conf

--- renew-certs.zsh
+++ renew-certs.zsh
@@ -2,4 +2,5 @@
 
 sudo certbot certonly --noninteractive --agree-tos --expand -m v@viktorh.net -d viktorh.net --webroot -w /home/viktor/viktorh
 sudo certbot certonly --noninteractive --agree-tos --expand -m v@viktorh.net -d viktor.pictures --webroot -w /home/viktor/watercolor
+sudo certbot certonly --noninteractive --agree-tos --expand -m v@viktorh.net -d helena.cool --webroot -w /home/helena/webs
 sudo service nginx reload
