c61f9eeb9ecdc0c2e836cc3e0ed071a9e6290055
Viktor <viktor@medino.com>
2023-02-11 17:36:17 +0000

    Add san pedro

--- create-previews.zsh
+++ create-previews.zsh
@@ -10,6 +10,6 @@ trap cleanup EXIT
 mkdir s/cropped
 
 for f in cropped/*;
-	do convert $f -resize 690x480 pnm:- | mozcjpeg -quality 85 > s/$f;
+	do convert $f -resize 690x480 pnm:- | cjpeg -quality 85 > s/$f;
 done
 mv s/cropped/* s
