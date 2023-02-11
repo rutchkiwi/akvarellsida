161d955bc44e6f82cfb62af92da0e1de7e4cd150
Viktor <viktor@medino.com>
2023-02-11 17:39:12 +0000

    new dep scir[pt

--- deploy.zsh
+++ deploy.zsh
@@ -1,1 +1,1 @@
-rsync -rv --update * raspberry:watercolor #move all files for watercolor site to raspberry
+rsync -rv --update * raspberry.lan:watercolor #move all files for watercolor site to raspberry
